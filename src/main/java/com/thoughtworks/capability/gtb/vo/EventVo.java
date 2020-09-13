66d58c7c63ba827109c9e54e91873cb5c88cb423
Doris8462 <liandou123>
2020-09-13 11:36:34 +0800

    [huangyunjie] feat:complete Problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,12 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.serializer.TimeDeserializer;
+import com.thoughtworks.capability.gtb.serializer.TimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +19,9 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonDeserialize(using = TimeDeserializer.class)
+  @JsonSerialize(using = TimeSerializer.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
